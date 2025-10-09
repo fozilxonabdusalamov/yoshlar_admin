0f0e47c4558f160553427b1ac81b2094e8784857
fozilxonabdusalamov <fozilxonabdusalamov2006@gmail.com>
2025-10-08 17:41:36 -0700

    update all

--- src/pages/AdminNewsPage.jsx
+++ src/pages/AdminNewsPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import NewsForm from '../components/NewsForm';
 import NewsList from '../components/NewsList';
 import './AdminNewsPage.css';
